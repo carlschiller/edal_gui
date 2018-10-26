c5b63ec293bc8cf48e164680fccdf3f046eea19b
carlschiller <carl.schiller@gmail.com>
2018-10-26 15:17:25 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/edal_gui.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/edal_gui.dir/main.cpp.o"
+  "edal_gui.pdb"
+  "edal_gui"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/edal_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/edal_gui.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for edal_gui.
+# This may be replaced when dependencies are built.
